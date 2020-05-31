5c8c6e466810a754762ddf0383560b0db5d79e87
leelic07 <lichengchao1@sina.cn>
2020-06-01 01:24:33 +0800

    2020-06-01 作业笔记

---
+++ code/app.js
@@ -0,0 +1,51 @@
+// var a = []
+
+// for(var i = 0; i < 10; i++) {
+//   a[i] = function() {
+//     console.log(i)
+//   }
+// }
+
+// a[6]()
+
+// var temp = 123
+
+// if(true) {
+//   console.log(temp)
+//   let temp
+// }
+
+// var arr = [ 12, 34, 32, 89, 4 ]
+
+// console.log(Math.min(...arr))
+
+// var a = 10
+
+// var obj = {
+//   a: 20,
+//   fn () {
+//     setTimeout(() => {
+//       console.log(this.a)
+//     })
+//   }
+// }
+
+// obj.fn()
+
+// var b = {
+//   [ Symbol() ]: '123'
+// }
+
+// Reflect.ownKeys(b).forEach((k) => console.log(k))
+
+
+// Promise.resolve('hello')
+// .then(res => {
+//   return res + 'lagou'
+// })
+// .then(res => {
+//   return res + 'I ❤ U'
+// })
+// .then(res => {
+//   console.log(res)
+// })
